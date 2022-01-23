3a3606d29a84e0e9f391a402a4d6a73f34e3df67
Felipe Baptista <felipebaptista@IT-NMC-FELBAPT1.local>
2022-01-23 18:16:41 -0300

    Pages Home and Login, Ok

--- project/src/Components/Header/index.jsx
+++ project/src/Components/Header/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 import './style.css';
 
 export default function Header() {
-    
+    let params =  useNavigate()
+
+    async function goLink() {
+        window.location.href = "https://www.disneyplus.com/pt-br/sign-up?type=standard"
+    }
+
+    function goLogin() {
+        params("/login")
+    }
+
     return(
         <main id="main_Header">
-            <img id='logo_Disney' src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original" alt="" />
+            <img id='logo_Disney' src="https://cnbl-cdn.bamgrid.com/assets/7ecc8bcb60ad77193058d63e321bd21cbac2fc67281dbd9927676ea4a4c83594/original"/>
             <section id="logo_Buttons">
-                <button className='logo-button-generic button-signature'>
-                    <a href="https://www.disneyplus.com/pt-br/sign-up?type=standard">ASSINE AGORA</a>
+                <button onClick={goLink} className='logo-button-generic button-signature'>
+                    ASSINE AGORA
                 </button>
-                <button className='logo-button-generic button-login'> 
+                <button onClick={goLogin} className='logo-button-generic button-login'> 
                     ENTRAR
                 </button>
             </section>
--- project/src/Pages/Home/index.jsx
+++ project/src/Pages/Home/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from "../../Components/Header";
 import WelcomeImage from "../../Images/disney+_Combo.png"
+import WelcomeSection1 from "../../Images/welcome_section1.png";
+import WelcomeSection2 from "../../Images/welcome_section2.jpeg";
+import IconTV from "../../Images/TV.png";
+import IconComputer from "../../Images/Computer.png";
+import IconPhone from "../../Images/Phone.png";
+import IconGames from "../../Images/Games.png";
 
 import './style.css';
 
 export default function Home() {
+
+    async function goLink() {
+        window.location.href = "https://www.disneyplus.com/pt-br/sign-up?type=bundle"
+    }
     
     return(
         <main id="main_Home">
@@ -13,7 +23,7 @@ export default function Home() {
                 <section id="welcome_Home">
                     <h1>Com o Combo+, você pode aproveitar o Disney+ e o Star+ por um preço único.</h1>
                     <img className='image-welcome' src={WelcomeImage}/>
-                    <button> 
+                    <button onClick={goLink}> 
                         ASSINE POR R$ 45,90*
                     </button>
                     <p>*O preço pode variar caso a assinatura seja feita através de outras plataformas.</p>
@@ -22,16 +32,74 @@ export default function Home() {
                 </section>
             </div>
             <div className='section-home'>
-
+                <img src={WelcomeSection1} className='image-section-division' />
+                <section className='welcome-content-section'>
+                    <h3>Assista do seu jeito</h3>
+                    <p>Aproveite a tela grande da TV ou assista no tablet, laptop, celular e outros aparelhos. Nossa seleção de títulos em 4K não para de crescer. Além disso, para a felicidade de todos, é possível assistir em até 4 telas ao mesmo tempo.</p>
+                </section>
             </div>
             <div className='section-home'>
-
+                <section className='welcome-content-section'>
+                    <p>Só o Combo+ oferece Disney+ e Star+ juntos para você ter acesso ilimitado a filmes, séries de sucesso e esportes ao vivo com a ESPN, para ver onde e como quiser.</p>
+                    <button onClick={goLink}>ASSINAR O COMBO+</button>
+                </section>
+                <img src={WelcomeSection2} className='image-section-division' />
             </div>
-            <div className='section-home'>
-
+            <div className='section-home background-image'>
+                <article className='division'></article>
+                <article className='division'>
+                    <h3>Baixe filmes e séries</h3>
+                    <p>Baixe e assista quando e onde quiser. Assim, seus favoritos estão sempre com você, até mesmo sem internet.</p>
+                </article>
             </div>
-            <div className='section-home'>
-
+            <div className='section-home section-column '>
+                <div id="device_Section">
+                    <h1 className='device-title'>Disponível nos seus dispositivos favoritos</h1>
+                    <section id='division_Device'>
+                        <article className='device-section'>
+                            <img src={IconTV} className='device-icon'/>
+                            <h3>TV</h3>
+                            <div className='device-content'>
+                                <p>Amazon Fire TV</p>
+                                <p>Android TV</p>
+                                <p>Apple TV</p>
+                                <p>Chromecast</p>
+                                <p>TVs LG</p>
+                                <p>Roku</p>
+                                <p>Samsung</p>
+                            </div>
+                        </article>
+                        <article className='device-section'>
+                            <img src={IconComputer} className='device-icon'/>
+                            <h3>Computador</h3>
+                            <div className='device-content'>
+                                <p>Chrome OS</p>
+                                <p>MacOS</p>
+                                <p>PC Windows</p>
+                            </div>
+                        </article>
+                        <article className='device-section'>
+                            <img src={IconPhone} className='device-icon'/>
+                            <h3>Celulares e tablets</h3>
+                            <div className='device-content'>
+                                <p>Tablets Amazon Fire</p>
+                                <p>Celulares e Tablets Android</p>
+                                <p>iPhone e iPad</p>
+                            </div>
+                        </article>
+                        <article className='device-section'>
+                            <img src={IconGames} className='device-icon'/>
+                            <h3>Videogames</h3>
+                            <div className='device-content'>
+                                <p>Ps4</p>
+                                <p>Ps5</p>
+                                <p>Xbox One</p>
+                                <p>Xbox Series X</p>
+                                <p>Xbox Series S</p>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </div>
         </main>
     )
